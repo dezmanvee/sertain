9a9874f206c652bc005bca17df633d3a25d48527
Bamidele Olaolu Ademokoya <bamyonline@gmail.com>
2025-07-30 13:25:38 +0100

    Server Action: Updating and Deleting Data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -54,3 +54,55 @@
     // Redirect to the invoices page
     redirect("/dashboard/invoices");
   }
+
+  // Function to update an invoice
+
+  export async function updateInvoice(id: string, formData: FormData) {
+
+    //Connect to the database
+    const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+    //capture form data
+    const rawData = Object.fromEntries(formData.entries());
+
+    //Validate form data with Zod
+    const FormSchema = z.object({
+      id: z.string(),
+      customerId: z.string(),
+      amount: z.coerce.number(),
+      status: z.enum(["pending", "paid"]),
+      date: z.string(),
+    });
+
+    //omit id and date from the schema
+    const UpdateInvoiceSchema = FormSchema.omit({ id: true, date: true });
+
+    //Parse the form data
+    const { customerId, amount, status } = UpdateInvoiceSchema.parse(rawData);
+    //Convert amount to cents
+    const amountInCents = amount * 100;
+
+    //update the invoice in the database
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}`;
+
+    //Revalidate the invoices cache
+    revalidatePath("/dashboard/invoices");
+
+    // Redirect to the invoices page
+    redirect("/dashboard/invoices");
+  }
+
+  // Function to delete an invoice
+  export async function deleteInvoice(id: string) {
+
+
+    //Delete the invoice from the database
+    await sql`
+      DELETE FROM invoices
+      WHERE id = ${id}`;
+
+    //Revalidate the invoices cache
+    revalidatePath("/dashboard/invoices");
+  }
