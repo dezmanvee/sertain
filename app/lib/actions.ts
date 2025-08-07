561283d386ea659639ea69085797328181af495d
Bamidele Olaolu Ademokoya <bamyonline@gmail.com>
2025-08-07 17:59:50 +0100

    Authentication: Credential provider

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,11 @@
     "use server";  
   
- import postgres from "postgres";
-  import { z } from "zod";
-  import { revalidatePath } from "next/cache";
-  import { redirect } from "next/navigation";
+import postgres from "postgres";
+ import { revalidatePath } from "next/cache";
+ import { z } from "zod";
+import { redirect } from "next/navigation";
+import { AuthError } from 'next-auth';
+import { signIn } from "../auth";
 
   export type State = {
     errors: {
@@ -160,4 +162,26 @@
 
     //Revalidate the invoices cache
     revalidatePath("/dashboard/invoices");
+
+    
   }
+
+
+  export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
